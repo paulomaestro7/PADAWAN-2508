2b5c614106f41faa49f6b8bc7aea5eeda76cda9f
André Vinícius Segall Ando <andre.ando@hbsis.com.br>
2020-08-26 15:46:24 -0300

    Formulário de cadastro de Cliente no grupo 5

---
+++ Grupo-5-WFA/FormCliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Grupo_5_WFA.Models;
+using Newtonsoft.Json;
+
+namespace Grupo_5_WFA
+{
+    public partial class FormCliente : Form
+    {
+        public FormCliente()
+        {
+            InitializeComponent();
+        }
+
+		private void btn_GravarCliente_Click(object sender, EventArgs e)
+		{
+            var caminho = "C:\\Users\\andre.ando\\Desktop\\Birulable.txt";
+            var cliente = new Models.Cliente(
+                txt_NomeCliente.Text,
+                txt_SobrenomeCliente.Text,
+                txt_CpfCliente.Text,
+				Convert.ToDateTime(txt_DataNascimentoCliente.Text));
+
+            using (StreamWriter writer = new StreamWriter(caminho, true)) 
+            {
+                var texto = JsonConvert.SerializeObject(cliente);
+                writer.WriteLine(texto);
+            }
+        }
+	}
+}
--- Grupo-5-WFA/Program.cs
+++ Grupo-5-WFA/Program.cs
@@ -17,7 +17,7 @@ namespace Grupo_5_WFA
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormCliente());
         }
     }
 }
