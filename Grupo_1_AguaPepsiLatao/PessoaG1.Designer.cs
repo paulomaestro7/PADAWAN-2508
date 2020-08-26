5fead5c7ddb8a950ed7181e6147a8eeac0f0e78f
Isabela Viegas Aguiar <isabela.aguiar@hbsis.com.br>
2020-08-26 16:45:51 -0300

    Commit

---
+++ Grupo_1_AguaPepsiLatao/PessoaG1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Grupo_1_AguaPepsiLatao
+{
+    partial class PessoaG1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.txt_Sobrenome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Gravar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.Location = new System.Drawing.Point(222, 91);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(250, 26);
+            this.txt_Nome.TabIndex = 0;
+            // 
+            // txt_Sobrenome
+            // 
+            this.txt_Sobrenome.Location = new System.Drawing.Point(222, 143);
+            this.txt_Sobrenome.Name = "txt_Sobrenome";
+            this.txt_Sobrenome.Size = new System.Drawing.Size(250, 26);
+            this.txt_Sobrenome.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sobrenome";
+            // 
+            // btn_Gravar
+            // 
+            this.btn_Gravar.Location = new System.Drawing.Point(514, 91);
+            this.btn_Gravar.Name = "btn_Gravar";
+            this.btn_Gravar.Size = new System.Drawing.Size(150, 78);
+            this.btn_Gravar.TabIndex = 5;
+            this.btn_Gravar.Text = "Gravar";
+            this.btn_Gravar.UseVisualStyleBackColor = true;
+            this.btn_Gravar.Click += new System.EventHandler(this.btn_Gravar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(231, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(231, 29);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Cadastro de Pessoa";
+            // 
+            // Pessoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btn_Gravar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Sobrenome);
+            this.Controls.Add(this.txt_Nome);
+            this.Name = "Pessoa";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Nome;
+        private System.Windows.Forms.TextBox txt_Sobrenome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Gravar;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Grupo_1_AguaPepsiLatao/UsuarioG1.cs
@@ -0,0 +1,31 @@
+﻿using CriacaoArquivo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grupo_1_AguaPepsiLatao
+{
+    public partial class UsuarioG1 : Form
+    {
+        public UsuarioG1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_GravarUsuario_Click(object sender, EventArgs e)
+        {
+            var usuario = new Cadastros.Usuario();
+
+            usuario.Login = txt_Login.Text;
+            usuario.Senha = txt_Senha.Text;
+
+            CriaArquivoG1.EscreverUsuario(usuario);
+        }
+    }
+}
