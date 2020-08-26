41d7efe53673512f0e5ba610ac0bdd9f95a5012a
Gabriel Henrique Dias Dainez <gabriel.dainezi@hbsis.com.br>
2020-08-26 16:23:10 -0300

    Commit

--- PADAWAN-2508/Form1.cs
+++ PADAWAN-2508/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,9 @@ namespace PADAWAN_2508
 {
     public partial class Form1 : Form
     {
+        string operador;
+        int a = 0;
+        bool validar = false;
         public Form1()
         {
             InitializeComponent();
@@ -66,31 +70,93 @@ namespace PADAWAN_2508
         {
             txt_Conta.Text = "";
             txt_Resultado.Text = "";
+            a = 0;
         }
 
         private void btn_Dividir_Click(object sender, EventArgs e)
         {
-            txt_Conta.Text += "/";
+            if (validar == true)
+            {
+                a = a / Convert.ToInt32(txt_Conta.Text);
+                txt_Resultado.Text = Convert.ToString(a) + "/";
+                txt_Conta.Text = "";
+                operador = "/";
+            }
+            else
+            {
+                txt_Resultado.Text = txt_Conta.Text + btn_Dividir.Text;
+                a = Convert.ToInt32(txt_Conta.Text);
+                txt_Conta.Text = "";
+                operador = "/";
+                validar = true;
+            }
         }
 
         private void btn_Multiplicar_Click(object sender, EventArgs e)
         {
-            txt_Conta.Text += "*";
+            if (validar == true)
+            {
+                a = a * Convert.ToInt32(txt_Conta.Text);
+                txt_Resultado.Text = Convert.ToString(a) + "*";
+                txt_Conta.Text = "";
+                operador = "*";
+            }
+            else
+            {
+                txt_Resultado.Text = txt_Conta.Text + btn_Multiplicar.Text;
+                a = Convert.ToInt32(txt_Conta.Text);
+                txt_Conta.Text = "";
+                operador = "*";
+                validar = true;
+            }
         }
 
         private void btn_Mais_Click(object sender, EventArgs e)
         {
-            txt_Conta.Text += "+";
+            if (validar == true)
+            {
+                a = a + Convert.ToInt32(txt_Conta.Text);
+                txt_Resultado.Text = Convert.ToString(a) + "+";
+                txt_Conta.Text = "";
+                operador = "+";
+            }
+            else
+            {
+                txt_Resultado.Text = txt_Conta.Text + btn_Mais.Text;
+                a = Convert.ToInt32(txt_Conta.Text);
+                txt_Conta.Text = "";
+                operador = "+";
+                validar = true;
+            }
         }
 
         private void btn_Menos_Click(object sender, EventArgs e)
         {
-            txt_Conta.Text += "-";
+            if (validar == true)
+            {
+                a = a - Convert.ToInt32(txt_Conta.Text);
+                txt_Resultado.Text = Convert.ToString(a) + "-";
+                txt_Conta.Text = "";
+                operador = "-";
+            }
+            else
+            {
+                txt_Resultado.Text = txt_Conta.Text + btn_Menos.Text;
+                a = Convert.ToInt32(txt_Conta.Text);
+                txt_Conta.Text = "";
+                operador = "-";
+                validar = true;
+            }
         }
 
         private void btn_Resultado_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+            txt_Conta.Text += (int)Numeros.zero;
         }
     }
     enum Numeros
