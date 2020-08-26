ee95b392dfd1b77833875f48988894eecff63d26
Rômulo Barbosa Ribeiro <romulo.ribeiro@hbsis.com.br>
2020-08-26 17:02:08 -0300

    Grupo4

---
+++ Grupo-4-WFA/CadastroClienteG4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Grupo_4_WFA
+{
+    public partial class CadastroClienteG4 : Form
+    {
+        public CadastroClienteG4()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Salvar_Click(object sender, EventArgs e)
+        {
+            var cliente = new ClienteJsonG4();
+            cliente.Nome = txt_Nome.Text;
+            cliente.Cpf = txt_Cpf.Text;
+            cliente.DataNasc = date_Nascimento.Value;
+            cliente.Escrever(cliente);
+        }
+    }
+}
