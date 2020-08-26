86085c5db05ba4837c8d91bb9aa957e09baac63a
Maestro <paulomaestro@employer.com.br>
2020-08-26 17:47:01 -0300

    #ENC - Modified project

--- Grupo-2-WFA/Program.cs
+++ Grupo-2-WFA/Program.cs
@@ -17,7 +17,7 @@ namespace Grupo_2_WFA
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Cliente());
         }
     }
 }
